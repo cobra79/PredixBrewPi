3df35d7fd07d2e100b3478df88968b5826a7ae4f
Marco Bradtke <marco.bradtke@ge.com>
2018-01-05 17:35:42 +0100

    creating random datapoints for real assets tags

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # PredixBrewPi
 
 Connect brewing kettles or fermenation equipment to the Predix cloud to monitor meshing or fermentation temperature.
+
+## Prepare the Raspberry
+
+Install the Predix Python SDK:
+pip install python
--- updateTsDemo.py
+++ updateTsDemo.py
@@ -1,5 +1,15 @@
 import predix.app
+import time
+import random
+
 app = predix.app.Manifest('manifest.yml')
 ts = app.get_timeseries()
-ts.queue('test', 123)
-ts.send()
+
+while True:
+    print('Send datapoint')
+    ts.queue('meshtun', random.uniform(40, 78))
+    ts.queue('wortkettle', random.uniform(95, 103))
+    ts.queue('fermentation', random.uniform(14, 25))
+    ts.queue('keezer', random.uniform(4,6))
+    ts.send()
+    time.sleep(20)
