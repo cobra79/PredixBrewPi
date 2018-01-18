01b9690a0d03d1a4dbf2de5c8926a8a5f4903352
Marco Bradtke <marco@bradtke.eu>
2018-01-18 21:18:48 +0000

    Sensor ids configured in manifest and real data is send to the Predix time series

--- updateTsDemo.py
+++ updateTsDemo.py
@@ -4,16 +4,35 @@ import time
 import random
 
 app = predix.app.Manifest('manifest.yml')
+
+
+
 ts = app.get_timeseries()
 
 while True:
-    print('Send datapoint')
-    sensor_1 = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "0517c1a4f1ff")
-    temp_in_c = sensor_1.get_temperature()
-    print("Sensor 1:", temp_in_c)
-    #ts.queue('meshtun', random.uniform(40, 78))
-    #ts.queue('wortkettle', random.uniform(95, 103))
-    #ts.queue('fermentation', random.uniform(14, 25))
-    #ts.queue('keezer', random.uniform(4,6))
-    #ts.send()
+    
+    mesh_kettle_sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, app.manifest['env']['MESH_KETTLE'])
+    wort_kettle_sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, app.manifest['env']['WORT_KETTLE'])
+    water_kettle_sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, app.manifest['env']['WATER_KETTLE'])
+    fermenter_sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, app.manifest['env']['FERMENTER'])
+    keezer_sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, app.manifest['env']['KEEZER'])
+    
+    mesh_temp_in_c = mesh_kettle_sensor.get_temperature()
+    wort_temp_in_c = wort_kettle_sensor.get_temperature()
+    water_temp_in_c = water_kettle_sensor.get_temperature()
+    fermenter_temp_in_c = fermenter_sensor.get_temperature()
+    keezer_temp_in_c = keezer_sensor.get_temperature()
+    
+    print("Mesh:", mesh_temp_in_c)
+    print("Wort:", wort_temp_in_c)
+    print("Water:", water_temp_in_c)
+    print("Fermenter:", fermenter_temp_in_c)
+    print("Keezer:", keezer_temp_in_c)
+    
+    ts.queue('meshtun', mesh_temp_in_c)
+    ts.queue('wortkettle', wort_temp_in_c)
+    ts.queue('waterkettle', water_temp_in_c)
+    ts.queue('fermentation', fermenter_temp_in_c)
+    ts.queue('keezer', keezer_temp_in_c)
+    ts.send()
     time.sleep(20)
