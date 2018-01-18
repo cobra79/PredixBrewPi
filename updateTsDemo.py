95df77e7c2b531272d287f6598c1e2003624d0aa
Marco Bradtke <marco@bradtke.eu>
2018-01-18 18:28:41 +0000

    added lib to read temp

--- updateTsDemo.py
+++ updateTsDemo.py
@@ -1,4 +1,5 @@
 import predix.app
+from  w1thermsensor import W1ThermSensor
 import time
 import random
 
@@ -7,9 +8,12 @@ ts = app.get_timeseries()
 
 while True:
     print('Send datapoint')
-    ts.queue('meshtun', random.uniform(40, 78))
-    ts.queue('wortkettle', random.uniform(95, 103))
-    ts.queue('fermentation', random.uniform(14, 25))
-    ts.queue('keezer', random.uniform(4,6))
-    ts.send()
+    sensor_1 = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, "0517c1a4f1ff")
+    temp_in_c = sensor_1.get_temperature()
+    print("Sensor 1:", temp_in_c)
+    #ts.queue('meshtun', random.uniform(40, 78))
+    #ts.queue('wortkettle', random.uniform(95, 103))
+    #ts.queue('fermentation', random.uniform(14, 25))
+    #ts.queue('keezer', random.uniform(4,6))
+    #ts.send()
     time.sleep(20)
